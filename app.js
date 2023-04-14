c31c825d33fb3e474b77f4d0e2ac5f363efa1bdb
Master Orji Jude Arinze <40861023+arinzeorji@users.noreply.github.com>
2023-04-14 01:00:42 +0100

    javascript, html, css and images uploaded

---
+++ app.js
@@ -0,0 +1,44 @@
+
+document.querySelector('.hamburger-menu').addEventListener('click', () => {
+    document.querySelector('.nav-wrapper').classList.toggle('clicked');
+})
+
+
+document.querySelector('.banner-btn').addEventListener('click', () => {
+    document.querySelector('.nav-wrapper').classList.add('clicked');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
